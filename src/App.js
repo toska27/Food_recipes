251a7d779562d43184e08c4d4863ea2a243bd44b
toska27 <nemanjatoskic58@gmail.com>
2024-04-25 16:37:13 +0200

    added router and photos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Recipes from "./pages/Recipes/Recipes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
