a44ec29180a5f042a08bdf3af6c77ed2ad26f1db
toska27 <nemanjatoskic58@gmail.com>
2024-07-08 19:08:40 +0200

    added btns for more and less

--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import RecipeListItem from "../RecipeListItem/RecipeListItem";
 import "./RecipeList.css";
 
 const RecipeList = ({ recipes, searchedQuery }) => {
+  const [count, setCount] = useState(6);
+
+  const search = (recipes) => {
+    return recipes.filter((recipe) => recipe.publisher);
+  };
+
+  let amount = parseInt(count);
+  let newSearch = search(recipes).splice(0, amount);
+
   return (
     <div className="recipeList">
       <h3 className="recipe_for">Recipe Lists for {searchedQuery}</h3>
       <div className="gridRecipes">
-        {recipes.map((recipe) => (
-          <RecipeListItem id={recipe.id} recipe={recipe} />
-        ))}
+        {newSearch.length > 0 ? (
+          newSearch.map((recipe) => {
+            return (
+              <RecipeListItem key={recipe.id} id={recipe.id} recipe={recipe} />
+            );
+          })
+        ) : (
+          <h2 className="no_matches">No matches</h2>
+        )}
+      </div>
+      <div className="btns_view">
+        <button
+          className={count <= newSearch.length ? "more_view" : "non_view"}
+          onClick={() => setCount(count + 10)}
+        >
+          Show more
+        </button>
+
+        <button
+          className={count > 10 ? "less_view" : "non_view"}
+          onClick={() => setCount(count - 10)}
+        >
+          Show less
+        </button>
       </div>
     </div>
   );
